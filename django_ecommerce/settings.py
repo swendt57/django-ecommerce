b39011dedf1f034924c9a455184d7c1ede2daf91
swendt57 <Lthr4ever>
2020-02-13 20:29:14 -0800

    corrected the heroku url in Allowed Hosts

--- django_ecommerce/settings.py
+++ django_ecommerce/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1', 'https://swendt-django-ecommerce.herokuapp.com/']
+ALLOWED_HOSTS = ['127.0.0.1', 'swendt-django-ecommerce.herokuapp.com/']
 
 
 # Application definition
